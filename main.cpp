2934fb4fadf380022c73d91404c2c94d70a3ec85
luizanegru <46330271+luizanegru@users.noreply.github.com>
2020-02-25 16:31:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,22 @@
 #include"trap.h"
 #include"game.h"
 
-
 using namespace std;
+
 int main()
 {
    int rows, cols;
    cout<<"Numarul de linii al hartii este: "<<endl;
    cin>>rows;
 
-    cout<<"Numarul de coloane al hartii este: "<<endl;
-    cin>>cols;
+   cout<<"Numarul de coloane al hartii este: "<<endl;
+   cin>>cols;
 
-    Game game;
-    Map map(rows, cols);
+   Game game;
+   Map map(rows, cols);
 
-    game.Initializare(game, map);
-    game.GamePlay(game, map);
-
-
-
+   game.Initializare(game, map);
+   game.GamePlay(game, map);
+   
   return 0;
 }
