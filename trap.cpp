8b048e3f88cd137164f2501d4786d4cc3245dbfd
luizanegru <46330271+luizanegru@users.noreply.github.com>
2020-02-25 16:46:34 +0200

    Update trap.cpp

--- trap.cpp
+++ trap.cpp
@@ -3,22 +3,18 @@
 //
 #include "trap.h"
 //constructor: pozitioneaza capcana pe harta
-Trap::Trap(int pozx, int pozy, Map &M)
-{
+Trap::Trap(int pozx, int pozy, Map &M) {
     M.SetPosition(pozx, pozy, trap);
     PositionTrap.first = pozx;
     PositionTrap.second = pozy;
 }
+
 //aduce robotul la pozitia initiala
 void Trap::functionalityTrap(Robot *rob, Map &M) {
-
-
     pair<int, int>pozR = rob->getPositionRobot();
     M.SetPosition(pozR.first, pozR.second, 0);
     M.SetPosition(0, 0, 1);
     rob->setPositionRobot(0, 0);
     pozR = rob->getPositionRobot();
-
-
 }
 
