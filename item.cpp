821f734b48d86f5bc7e06a31903b7813941a29b0
luizanegru <46330271+luizanegru@users.noreply.github.com>
2020-02-25 16:29:14 +0200

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -4,20 +4,19 @@
 
 #include"item.h"
 
-Item::Item(int pozx, int pozy, char comp, Map &M){
-
+Item::Item(int pozx, int pozy, char comp, Map &M) {
     M.SetPosition(pozx, pozy, item);
     PositionItem.first = pozx;
     PositionItem.second = pozy;
     compatibility = comp;
-
 }
 
 ///////// Class Item for Robot Mojo ////////////
 
-ItemMojo::ItemMojo(int pozx, int pozy, char comp, Map &M) : Item(pozx, pozy, comp, M){
+ItemMojo::ItemMojo(int pozx, int pozy, char comp, Map &M) : Item(pozx, pozy, comp, M) {
 
 }
+
 //acest item are functionalitatea de a muta robotul cu doi pasi mai la dreapta, daca nu se poate atunci cu un pas mai in jos
 void ItemMojo::functionalityItem(Robot *rob, Map &M) {
 
@@ -29,8 +28,7 @@ void ItemMojo::functionalityItem(Robot *rob, Map &M) {
             rob->setPositionRobot(pozR.first, pozR.second + 2);
         }
 
-            else if(pozR.first + 2 < M.GetRows())
-            {
+            else if(pozR.first + 2 < M.GetRows()) {
                 M.SetPosition(pozR.first + 2, pozR.second, 1);
                 rob->setPositionRobot(pozR.first + 2, pozR.second);
              }
@@ -40,7 +38,6 @@ void ItemMojo::functionalityItem(Robot *rob, Map &M) {
 
 ///////// Class Item for Robot Jojo ////////////
 ItemJojo::ItemJojo(int pozx, int pozy, char comp, Map &M) : Item(pozx, pozy, comp, M){
-
 }
 
 //acest item are functionalitatea de a muta robotul un pas pe diagonala in jos
@@ -68,16 +65,13 @@ void ItemCojo::functionalityItem(Robot *rob, Map &M) {
     pair<int, int>pozT = M.GetTreasure();
     M.SetPosition(pozR.first, pozR.second, 0);
 
-    if( pozT.second - 1 < M.GetCols())
-    {
+    if( pozT.second - 1 < M.GetCols()) {
         M.SetPosition(pozR.first, pozT.second - 1, 1);
         rob->setPositionRobot(pozR.first, pozT.second + 1);
     }
 
-        else if(pozT.second + 1 < M.GetCols())
-        {
+        else if(pozT.second + 1 < M.GetCols()) {
         M.SetPosition(pozR.first, pozT.second + 1, 1);
         rob->setPositionRobot(pozR.first, pozT.second + 1);
         }
-
 }
