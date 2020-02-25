a715688d476cf4f75700d03afaa76ed69f12eb06
luizanegru <46330271+luizanegru@users.noreply.github.com>
2020-02-25 16:47:35 +0200

    Update trap.h

--- trap.h
+++ trap.h
@@ -1,37 +1,32 @@
 //
 // Created by Luiza Negru on 2019-04-23.
 //
-
 #ifndef UNTITLED4_TRAP_H
 #define UNTITLED4_TRAP_H
 
 #include"item.h"
-
 //Trap
 #define trap -1
 
-class Trap{
-
+class Trap {
 protected:
     pair<int, int>PositionTrap;
 
 public:
     Trap(int pozx, int pozy , Map &M);
 
-    pair<int, int>GetPositionTrap() const{
+    pair<int, int>GetPositionTrap() const {
         return PositionTrap;
     }
 
     void functionalityTrap(Robot *rob, Map &M);
 
-    void setPositionTrap(int row, int col)
-    {
+    void setPositionTrap(int row, int col) {
         PositionTrap.first = row;
         PositionTrap.second = col;
     }
 
-    pair<int, int> GetPositionTrap()
-    {
+    pair<int, int> GetPositionTrap() {
         return PositionTrap;
     }
 };
