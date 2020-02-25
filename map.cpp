09e64b26869631ebeb860566cabea136496880c1
luizanegru <46330271+luizanegru@users.noreply.github.com>
2020-02-25 16:34:34 +0200

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -4,41 +4,35 @@ using namespace std;
 //definim Itemul egal cu 3
 
 //constructor cu doi parametrii
-
-Map::Map(int r, int c)
-{
+Map::Map(int r, int c) {
     rows = r;
     cols = c;
     int i;
     map = new int *[rows];
 
-    for (i = 0; i < rows; i++)
-    {
+    for (i = 0; i < rows; i++) {
         map[i] = new int[cols];
     }
 
-    for (i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             map[i][j] = 0;
         }
     }
 }
+
 //destructor al hartii
-Map::~Map(){
-    for(int i = 0; i < rows; i++)
-    {
+Map::~Map() {
+    for(int i = 0; i < rows; i++) {
         delete[] map[i];
     }
     delete[] map;
 }
+
 //metoda care afiseaza matricea
-void Map::Show(){
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+void Map::Show() {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             cout << map[i][j] << " ";
         }
         cout << endl;
@@ -47,16 +41,18 @@ void Map::Show(){
 
 bool Map::IsItem(int pozx, int pozy) const {
 
-    if(map[pozx][pozy] == item)
+    if(map[pozx][pozy] == item) {
         return true;
+    }
 
-            return false;
+     return false;
 }
 
 bool Map::IsTrap(int pozx, int pozy) const {
 
-    if(map[pozx][pozy] == trap)
+    if(map[pozx][pozy] == trap) {
         return true;
-
+    }
+    
     return false;
 }
